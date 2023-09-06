0a212376c286ab87ea28745b4f92f10299181574
FahrianAfdholi <16521492@mahasiswa.itb.ac.id>
2023-09-06 17:55:16 +0700

    pre final 1.1

--- order-service/common/config/config.go
+++ order-service/common/config/config.go
@@ -2,8 +2,10 @@ package config
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/order-service/common/service"
 	"github.com/order-service/db"
+	"sync"
 )
 
 type PaymentService struct {
@@ -60,5 +62,98 @@ func (ps *PaymentService) FindId(_ context.Context, in *service.PaymentId) (*ser
 		return nil, err
 	}
 
+	defer _db.Conn.Close()
 	return res, nil
+}
+
+func (ps *PaymentService) AddPayment(_ context.Context, in *service.PaymentRequest) (*service.PaymentResponse, error) {
+	var (
+		_db db.Database
+	)
+	_db.Connect()
+
+	if _, err := _db.Conn.Exec("INSERT INTO payment VALUES (?,?,?,?,?,?,?,?,?,?)",
+		uuid.New(), in.GetCustomerId(), in.GetCustomerName(),
+		in.GetProductID(), in.GetProductName(), in.GetPrice(),
+		in.GetDateOrder(), in.GetServiceShipmentName(),
+		in.GetDateShipment(), in.GetShipmentMethod(),
+	); err != nil {
+		return nil, err
+	}
+
+	defer _db.Conn.Close()
+	return &service.PaymentResponse{Status: "ok"}, nil
+}
+
+func (ps *PaymentService) AddPayments(ctx context.Context, in *service.PaymentsResponse) (*service.PaymentResponse, error) {
+	var wg sync.WaitGroup
+
+	errCh := make(chan error, len(in.GetPayments()))
+
+	wg.Add(len(in.GetPayments()))
+
+	for _, value := range in.GetPayments() {
+		go func(payment *service.PaymentRequest, c context.Context) {
+			defer wg.Done()
+			if _, err := ps.AddPayment(c, payment); err != nil {
+				errCh <- err
+			}
+		}(value, ctx)
+	}
+	wg.Wait()
+
+	close(errCh)
+
+	var allErrors []error
+	for err := range errCh {
+		allErrors = append(allErrors, err)
+	}
+	if allErrors[0] != nil {
+		return nil, allErrors[0]
+	}
+
+	return &service.PaymentResponse{Status: "ok"}, nil
+}
+
+func (ps *PaymentService) DeletePayment(_ context.Context, in *service.PaymentId) (*service.PaymentResponse, error) {
+	var _db db.Database
+	_db.Connect()
+
+	if _, err := _db.Conn.Exec("DELETE FROM payment WHERE id = ?", in.GetId()); err != nil {
+		return nil, err
+	}
+
+	defer _db.Conn.Close()
+
+	return &service.PaymentResponse{Status: "ok"}, nil
+}
+
+func (ps *PaymentService) DeletePayments(ctx context.Context, in *service.PaymentIds) (*service.PaymentResponse, error) {
+	var wg sync.WaitGroup
+
+	wg.Add(len(in.GetPaymentId()))
+	errCh := make(chan error, len(in.GetPaymentId()))
+
+	for _, value := range in.GetPaymentId() {
+		go func(id *service.PaymentId, c context.Context) {
+			defer wg.Done()
+			if _, err := ps.DeletePayment(c, id); err != nil {
+				errCh <- err
+			}
+		}(value, ctx)
+	}
+
+	wg.Wait()
+
+	close(errCh)
+
+	var allErrors []error
+	for err := range errCh {
+		allErrors = append(allErrors, err)
+	}
+	if allErrors[0] != nil {
+		return nil, allErrors[0]
+	}
+
+	return &service.PaymentResponse{Status: "ok"}, nil
 }
--- order-service/main.go
+++ order-service/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"github.com/order-service/common/config"
+	"github.com/order-service/common/service"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	srv := grpc.NewServer()
+	product := &config.PaymentService{}
+
+	fmt.Println("listening port:", port)
+
+	service.RegisterPaymentServiceServer(srv, product)
+	srv.Serve(lis)
+}
